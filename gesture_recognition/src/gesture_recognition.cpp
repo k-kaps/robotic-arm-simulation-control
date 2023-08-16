bd0b0edbfad326e2626ee0a799db94b4f629ff85
k-kaps <k-kaps@github.com>
2023-08-16 09:43:09 -0400

    modified gesture recognition file

---
+++ gesture_recognition/src/gesture_recognition.cpp
@@ -0,0 +1,12 @@
+#include "gesture_recognition/gesture_recognition.hpp"
+
+GestureRecognition::GestureRecognition(){
+    gesture = "";
+}
+
+std::string GestureRecognition::findGesture(sensor_msgs::msg::Image::SharedPtr image_msg){
+    // TODO : find the gesture from the image
+    gesture = "some gesture";
+    return gesture;
+}
+>>>>>>> master
